445b322642dd2d1f8a70ce2b888b6cba90e66613
lapshuntanya <lapshun.tatiana@gmail.com>
2025-01-28 19:03:17 +0200

    add main

--- P42-Lesson-18-Task1.cpp
+++ P42-Lesson-18-Task1.cpp
@@ -1,8 +1,17 @@
-#include <iostream>
+#include "Computer.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Computer* arr = new Computer[3]{
+        {"HP", 2.4, 8, false, 25000},
+        {"Asus", 3.2, 16, true, 65999},
+        {"Aser", 1.2, 4, false, 7999}
+    };
+
+    for (int i = 0; i < 3; i++)
+    {
+        arr[i].showInfo();
+    }
 }
 
 
