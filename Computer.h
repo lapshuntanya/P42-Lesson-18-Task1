c832f3081977f75995c52443981328dcd5fd29e7
lapshuntanya <lapshun.tatiana@gmail.com>
2025-01-28 18:43:19 +0200

    Add struct Computer

---
+++ Computer.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include <iostream>
+#include <cstring>
+#include <iomanip>
+using namespace std;
+
+struct Computer
+{
+	char name[100] = "undefined";	//•	Назва
+	float frequency = 0;			//•	Частота
+	int RAM = 0;					//•	Обсяг оперативної пам'яті
+	bool DVD_ROM = false;			//•	Наявність DVD ROM
+	float price = 0;				//•	Вартість
+
+	void showInfo() {
+		cout << setw(15) << "Name: " << name << endl;
+		cout << setw(15) << "Frequency: " << frequency << "GHz" << endl;
+		cout << setw(15) << "RAM: " << RAM << "Gb" << endl;
+		cout << setw(15) << "DVD ROM: " << boolalpha << DVD_ROM << endl;
+		cout << setw(15) << "Price: " << price << "UAH" << endl;
+		cout << "--------------------------------------------------\n";
+	}
+};
